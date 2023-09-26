9d8763d4def2f95b542689bfd26e2b96eb5e41d1
kadir.amini <abdhas2@gmail.com>
2023-09-26 14:16:08 +0200

    made database.py and made a test file to check the files.

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,20 +1,30 @@
-#hvordan koble opp mot database serveren med SQLALCHEMY som bruker ORM og man slipper å kode sql
-#ORM står for OBJECTIV REALSION MODELL
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import sqlite3
 
-#nøvendig for å koble opp sqlalchemy
-DATABASE_URL = "mysql+pymysql://root:Delnia123!@localhost:3306/gruppeoppgave"
-engine = create_engine(DATABASE_URL)
-sessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
-Base = declarative_base()
+DATABASE_NAME = "mydatabase.db"
+
+def get_database_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_tables():
+    conn = get_database_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS buyers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS sellers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            company TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 
-#depandancy, kobler opp her
-def get_db():
-    db = sessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,28 +1,9 @@
-#her skal vi ha modelen vår for databsen
+from pydantic import BaseModel
 
-from sqlalchemy import Column, Integer, String, Boolean, Text, Float
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.sql.expression import text
-from database import Base
+class Buyer(BaseModel):
+    name: str
+    email: str
 
-#her lager vi tabellen: veldig grei oppsett som kan refereser tilbake til mysqlworkbench
-class Guid(Base):
-    __tablename__ = "guids"
-
-    id = Column(Integer, primary_key= True,autoincrement=True, nullable= False)
-    pris = Column(Float)
-    title = Column(String(255), nullable= False)
-    content = Column(Text, nullable=False)
-    sted = Column(String(255), nullable=False)
-    #created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    user_id = Column(Integer)
-
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key= True,autoincrement=True, nullable= False)
-    fNavn = Column(String(255), nullable=False)
-    eNavn = Column(String(255), nullable= False)
-    fodselsnummer = Column(Integer, nullable=False)
+class Seller(BaseModel):
+    name: str
+    company: str
