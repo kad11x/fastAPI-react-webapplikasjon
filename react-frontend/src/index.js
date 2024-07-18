e1ffe4e6f618ee4d3585d05dc4f0282741b5f0a9
kadir <111745953+kad11x@users.noreply.github.com>
2024-07-18 18:08:44 +0200

    changed to react vite on the frontend

--- react-frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
