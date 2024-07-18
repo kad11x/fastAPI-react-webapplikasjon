dda80237c0942f47888dc1cbaf162b89cb00cf02
kadir <111745953+kad11x@users.noreply.github.com>
2024-07-19 00:27:45 +0200

    login fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './pages/styling/Login.css';
 
 ReactDOM.render(
   <React.StrictMode>
